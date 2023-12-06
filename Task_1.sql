b2fe49d8af57da1ada5c4a56350ad5d1d8334062
KaterinaPekarova <145158133+KaterinaPekarova@users.noreply.github.com>
2023-12-06 21:20:43 +0100

    Update Task_1.sql

--- Task_1.sql
+++ Task_1.sql
@@ -1,26 +1,25 @@
--- úkol 1 Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+ -- úkol 1 Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+WITH yearly_payroll AS (
+    SELECT
+        industry_branch_name,
+    	payroll_year,
+        AVG(payroll_value) AS avg_payroll,
+        LAG(AVG(payroll_value)) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year) AS previous_avg_payroll
+    FROM
+        t_katerina_pekarova_project_SQL_primary_final
+    GROUP BY
+        industry_branch_name, payroll_year
+)
 SELECT
-  industry_branch_name,
-  MIN(payroll_year) AS min_year,
-  MAX(payroll_year) AS max_year,
-  AVG(CASE WHEN payroll_year = MINYear THEN payroll_value END) AS avg_payroll_min_year,
-  AVG(CASE WHEN payroll_year = MAXYear THEN payroll_value END) AS avg_payroll_max_year,
-  CASE WHEN AVG(CASE WHEN payroll_year = MINYear THEN payroll_value END) <
-            AVG(CASE WHEN payroll_year = MAXYear THEN payroll_value END) THEN 'Increasing'
-       WHEN AVG(CASE WHEN payroll_year = MINYear THEN payroll_value END) >
-            AVG(CASE WHEN payroll_year = MAXYear THEN payroll_value END) THEN 'Decreasing'
-       ELSE 'Stable' END AS trend
-FROM (
-  SELECT
-    industry_branch_name,
+	industry_branch_name,
     payroll_year,
-    payroll_value,
-    MIN(payroll_year) OVER (PARTITION BY industry_branch_name) AS MINYear,
-    MAX(payroll_year) OVER (PARTITION BY industry_branch_name) AS MAXYear
-  FROM
-    t_katerina_pekarova_project_SQL_primary_final
-) AS Subtable
-GROUP BY
-  industry_branch_name
+    avg_payroll AS payroll_value,
+    CASE
+        WHEN avg_payroll > previous_avg_payroll THEN 'Increasing'
+        WHEN avg_payroll < previous_avg_payroll THEN 'Decreasing'
+        ELSE 'Stable'
+    END AS payroll_trend
+FROM
+    yearly_payroll
 ORDER BY
-  industry_branch_name;
+    industry_branch_name, payroll_year;
