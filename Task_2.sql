3e83dad629365d7f3713d8486e7e0b271e7128cb
KaterinaPekarova <145158133+KaterinaPekarova@users.noreply.github.com>
2023-12-06 22:17:09 +0100

    Task_2.sql

--- Task_2.sql
+++ Task_2.sql
@@ -1,23 +1,27 @@
 -- úkol 2 Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
-WITH food_data AS (
+WITH selected_years AS (
     SELECT
-        tl.payroll_year,
-        AVG(CASE WHEN tl.category_code = '114201' THEN tl.price_value ELSE 0 END) AS avg_milk_price,
-        AVG(CASE WHEN tl.category_code = '111301' THEN tl.price_value ELSE 0 END) AS avg_bread_price,
-        tl.payroll_value
+        payroll_year,
+        SUM(payroll_value) AS total_payroll,
+        AVG(CASE WHEN category_code = '114201' THEN price_value END) AS avg_milk_price,
+        AVG(CASE WHEN category_code = '111301' THEN price_value END) AS avg_bread_price
     FROM
-        t_katerina_pekarova_project_SQL_primary_final tl
+        t_katerina_pekarova_project_SQL_primary_final
     WHERE
-        tl.payroll_year IN (2006, 2018)
+        payroll_year IN (2006, 2018)
     GROUP BY
-        tl.payroll_year, tl.payroll_value
+        payroll_year
 )
 SELECT
-    d.payroll_year,
-   	(ROUND(d.payroll_value / d.avg_milk_price, 2)) AS liters_of_milk,
-    (ROUND(d.payroll_value / d.avg_bread_price, 2)) AS kilograms_of_bread
+    sy.payroll_year,
+    SUM(sy.total_payroll) AS total_payroll,
+    ROUND(AVG(sy.avg_milk_price), 2) AS avg_milk_price,
+    ROUND(AVG(sy.avg_bread_price), 2) AS avg_bread_price,
+    ROUND(SUM(sy.total_payroll) / AVG(sy.avg_milk_price), 2) AS liters_of_milk,
+    ROUND(SUM(sy.total_payroll) / AVG(sy.avg_bread_price), 2) AS kilograms_of_bread
 FROM
-    food_data d
-GROUP BY payroll_year 
+    selected_years sy
+GROUP BY
+    sy.payroll_year
 ORDER BY
-    d.payroll_year;
+    sy.payroll_year;
